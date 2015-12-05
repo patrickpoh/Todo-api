b46b74cc43ffc36465cf563b60ff6b76604b528f
Patrick Poh <patrickpoh91@gmail.com>
2015-12-05 18:47:03 +0800

    Final static todos

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res){
 	res.send('Todo API Root');
 });
 
-// GET /todos
+// GET /todos?completed=true&q=house
 app.get('/todos', function(req,res){
 	var queryParams = req.query;
 	var filteredTodos = todos;
@@ -20,6 +20,12 @@ app.get('/todos', function(req,res){
 		filteredTodos = _.where(filteredTodos, {completed: true});
 	}else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false'){
 		filteredTodos = _.where(filteredTodos, {completed: false});
+	}
+
+	if(queryParams.hasOwnProperty('q')&&queryParams.q.length>0){
+		filteredTodos = _.filter(filteredTodos, function (todo){
+			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase())> -1; //return the position of the word if it exist
+		});
 	}
 
 	res.json(filteredTodos); //this to do array is going to be converted to json
