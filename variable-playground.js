e90dea45c63a5738e4f6762dbfbe0670aebe964f
Patrick Poh <patrickpoh91@gmail.com>
2015-12-05 18:27:26 +0800

    Add variable-playground area

---
+++ variable-playground.js
@@ -0,0 +1,22 @@
+// var person = {
+// 	name: 'Patrick',
+// 	age: 24
+// }
+
+// function updatePerson(obj){
+// 	obj.age = 50;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+var grades = [15, 88];
+
+function addGrades (grades){
+	grades.push(55);
+	debugger;
+}
+
+addGrades(grades);
+
+console.log(grades);
