6fbe6440b649b012272fe3e46337037deb5ec858
Patrick Poh <patrickpoh91@gmail.com>
2015-12-07 16:11:39 +0800

    Convert GET /todos to sequelize

--- server.js
+++ server.js
@@ -14,26 +14,44 @@ app.get('/', function(req, res) {
 
 // GET /todos?completed=true&q=house
 app.get('/todos', function(req, res) {
-	var queryParams = req.query;
-	var filteredTodos = todos;
+	var query = req.query;
+	var where = {};
 
-	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
-		filteredTodos = _.where(filteredTodos, {
-			completed: true
-		});
-	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
-		filteredTodos = _.where(filteredTodos, {
-			completed: false
-		});
+	if(query.hasOwnProperty('completed') && query.completed === 'true'){
+		where.completed = true;
 	}
-
-	if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
-		filteredTodos = _.filter(filteredTodos, function(todo) {
-			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1; //return the position of the word if it exist
-		});
+	else if (query.hasOwnProperty('completed') && query.completed === 'false'){
+		where.completed = false;
 	}
+	if(query.hasOwnProperty('q') && query.q.length > 0){
+		where.description = {
+			$like: '%' +query.q + '%'
+		};
+	}
+	db.todo.findAll({where: where}).then(function(todos){
+		res.json(todos);
+	}, function(e){
+		res.status(500).send();
+	});
+	// var filteredTodos = todos;
 
-	res.json(filteredTodos); //this to do array is going to be converted to json
+	// if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+	// 	filteredTodos = _.where(filteredTodos, {
+	// 		completed: true
+	// 	});
+	// } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+	// 	filteredTodos = _.where(filteredTodos, {
+	// 		completed: false
+	// 	});
+	// }
+
+	// if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+	// 	filteredTodos = _.filter(filteredTodos, function(todo) {
+	// 		return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1; //return the position of the word if it exist
+	// 	});
+	// }
+
+	// res.json(filteredTodos); //this to do array is going to be converted to json
 });
 // GET /todos/:id
 app.get('/todos/:id', function(req, res) {
