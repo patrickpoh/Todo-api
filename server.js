24f29cbbd4122445cbe0ec3f61ca1a14846c8081
Patrick Poh <patrickpoh91@gmail.com>
2015-12-07 17:30:19 +0800

    Convert DELETE /todos/:id to sequelize

--- server.js
+++ server.js
@@ -107,18 +107,35 @@ app.post('/todos', function(req, res) {
 //DELETE /todos/:id
 app.delete('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {
-		id: todoId
+	
+	db.todo.destroy({
+		where: {
+			id: todoId
+		}
+	}).then(function (rowsDeleted){
+		if(rowsDeleted === 0){
+			res.status(404).json({
+				error: 'No todo with id'
+			});
+		}else{
+			res.status(204).send();  //204 means everything went well and there is nothing to send back
+		}
+	}, function(){
+		res.status(500).send();
 	});
 
-	if (!matchedTodo) {
-		res.status(404).json({
-			"error": "no todo found with that id"
-		});
-	} else {
-		todos = _.without(todos, matchedTodo);
-		res.json(matchedTodo);
-	}
+	// var matchedTodo = _.findWhere(todos, {
+	// 	id: todoId
+	// });
+
+	// if (!matchedTodo) {
+	// 	res.status(404).json({
+	// 		"error": "no todo found with that id"
+	// 	});
+	// } else {
+	// 	todos = _.without(todos, matchedTodo);
+	// 	res.json(matchedTodo);
+	// }
 });
 //PUT /todos/:id
 app.put('/todos/:id', function(req, res) {
