5b4916177541554519e67b4b8f73b1b2a2825b24
Patrick Poh <patrickpoh91@gmail.com>
2015-12-08 11:45:19 +0800

    Add login route and authenticate class method

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
 var db = require('./db.js');
+var bcrypt = require('bcryptjs');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [];
@@ -176,7 +178,35 @@ app.post('/users', function (req, res){
 	});
 });
 
-db.sequelize.sync().then(function() {
+//POST /users/login
+app.post('/users/login', function (req, res){
+	var body = _.pick(req.body, 'email', 'password');
+
+	db.user.authenticate(body).then(function (user){
+		res.json(user.toPublicJSON());
+	}, function (){
+		res.status(401).send();
+	});
+	// if(typeof body.email !== 'string' || typeof body.password !== 'string'){
+	// 	return res.status(400).send();
+	// }
+
+	// db.user.findOne({
+	// 	where: {
+	// 		email: body.email
+	// 	}
+	// }).then(function (user){
+	// 	if(!user || !bcrypt.compareSync(body.password, user.get('password_hash'))){
+	// 		return res.status(401).send(); //401 means autehntication is possible but it failed
+	// 	}
+	// 	res.json(user.toPublicJSON());
+	// }, function (e){
+	// 	res.status(500).send();
+	// });
+});
+
+
+db.sequelize.sync({force :true}).then(function() {
 	app.listen(PORT, function() {
 		console.log('Express listening on port ' + PORT + '!');
 	});
